ceda0140d630a4b118a9c46ed175486260db7013
Kostiantyn Pavlyshyn <127691807+Kostia20034@users.noreply.github.com>
2026-07-06 22:49:14 -0500

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.length() > s2.length()) return false;
+
+        vector<int> s1Count(26,0);
+        vector<int> s2Count(26,0);
+
+        for(int i = 0; i < s1.length(); i++){
+            s1Count[s1[i] - 'a']++;
+            s2Count[s2[i] - 'a']++;
+        }
+
+        int matches = 0;
+        
+        for(int i = 0; i < 26;i++){
+            if(s1Count[i] == s2Count[i]) matches++;
+        }
+        if(matches == 26) return true;
+
+        int l = 0;
+        for(int r = s1.length(); r < s2.length(); r++){
+            s2Count[s2[r] - 'a']++;
+            if(s2Count[s2[r] - 'a'] == s1Count[s2[r] - 'a']) matches++;
+            else if(s2Count[s2[r] - 'a'] == s1Count[s2[r] - 'a'] + 1) matches--;
+
+            s2Count[s2[l] - 'a']--;
+            if(s2Count[s2[l] - 'a'] == s1Count[s2[l] - 'a']) matches++;
+            else if(s2Count[s2[l] - 'a'] == s1Count[s2[l] - 'a'] - 1) matches--;
+
+            if(matches == 26) return true;
+            l++;
+
+        }
+        return false;
+        
+    }
+};
