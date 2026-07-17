c66058dc922fb92bba950f9e2dc50fc8c4f0be8b
Kostiantyn Pavlyshyn <127691807+Kostia20034@users.noreply.github.com>
2026-07-16 21:48:48 -0500

    Add: copy-linked-list-with-random-pointer - submission-1

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-1.cpp
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(head == nullptr) return head;
+        unordered_map<Node*,Node*> nodeMap;
+        Node* temp = head;
+        for(;temp != nullptr;){
+            nodeMap[temp] = new Node(temp->val);
+            temp = temp->next;
+        }
+
+        temp = head;
+        for(;temp != nullptr;){
+            nodeMap[temp]->next = nodeMap[temp->next];
+            nodeMap[temp]->random = nodeMap[temp->random];
+            temp = temp->next;
+        }
+        return nodeMap[head];
+        
+    }
+};
